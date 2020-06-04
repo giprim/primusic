04c93891fbedea4c1ebdf2fdfba78ca424d0a387
giprim <gi4real.gi@gmail.com>
2020-06-04 02:17:45 +0100

    removed loaderReducer and all related to it as it is not needed anymore

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,10 +1,8 @@
 import search from './searchReducer';
 import theme from './themeReducer';
-import loaded from './loadedReducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
 	search,
 	theme,
-	loaded,
 });
